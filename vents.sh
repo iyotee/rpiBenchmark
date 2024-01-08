4dbd891a993ee7583ba28cbe4b2ba3fb12eb92aa
iyotee <jeremy.noverraz@gmail.com>
2024-01-08 19:22:03 +0100

    Version 1.1

--- vents.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Get the fan speed
-fan_speed=$(cat /sys/class/thermal/thermal_zone0/temp)
-
-# Print the fan speed in RPM (you may need to adjust the conversion factor)
-echo "Fan Speed: $((fan_speed / 1000)) RPM"
